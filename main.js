766073ae875130a864bb8e6bb1424c4ad6e25784
isadora <125563393+isa2405@users.noreply.github.com>
2024-04-16 11:41:47 -0300

    res

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
-
+    var respostaTime = prompt("Qual minha data de aniversari?");
+    var respostaTime=prompt("Qual meu signo");
+    var respostaTime=prompt("Qual é meu time ?);
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
+    if (respostaTime.toLowerCase() === "24/05)"{
       alert("Isso mesmo! O Athlético PR é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
+  
     }
   }
 }
